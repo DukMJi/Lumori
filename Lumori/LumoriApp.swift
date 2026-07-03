ba5fd684a47312ea95d9e221fcce5c104b9592f8
Duk Ji <dukmji@gmail.com>
2026-07-02 21:20:19 -0500

    Initial Commit

---
+++ Lumori/LumoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoriApp.swift
+//  Lumori
+//
+//  Created by Tommy Ji on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LumoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
